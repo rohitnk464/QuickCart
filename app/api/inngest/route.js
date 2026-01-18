8f60eb8f08c05c0a9850ec5c9e9d45e9a13edc3d
Rohit M Naik <rohitmnaik9@gmail.com>
2026-01-19 01:07:17 +0530

    Fix missing Inngest import

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,11 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+} from "@/config/inngest";
 
-// Create an API that serves functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
